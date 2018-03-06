d0270229a6edc90fbd1b9723731b014ebf8763ca
lzccheng <lai.zhicheng@yahoo.com>
2018-03-07 00:06:08 +0800

    gai

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -95,19 +95,14 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang='sass' scoped>
+  img
+    border-radius: 10px
+  a
+    color: red
+    text-decoration: none
+  ul
+    list-style: none
+  
+  
 </style>
