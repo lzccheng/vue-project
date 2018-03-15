2664ceb59a6d59c8afbdad283c38d56fbb843929
lzccheng <lai.zhicheng@yahoo.com>
2018-03-16 00:44:27 +0800

    first

---
+++ vue/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button @click='click'>click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    click: function () {
+      console.log(this.$http)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang='sass' scoped>
+  button
+    color: red
+</style>
